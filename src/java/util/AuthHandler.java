fb0ee684c5e041f5e7f73f2d605b0bdba703efec
mrkschan <mr.kschan@gmail.com>
2010-02-05 12:32:53 +0800

    strengthen auth check

---
+++ src/java/util/AuthHandler.java
@@ -0,0 +1,31 @@
+package util;
+
+import config.Config;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthHandler {
+
+    public static String checkAuth(HttpServletRequest request, HttpServletResponse response)
+    throws IOException {
+
+        HttpSession s = request.getSession(true);
+
+        if (null == s.getAttribute("user")) {
+            Config c = new Config();
+            String auth_svr = c.getConfig("auth_server_url");
+
+            response.sendRedirect(auth_svr +
+                "?app_auth=" + request.getContextPath() + "/auth" +
+                "&app_ip=" + request.getLocalAddr() +
+                "&app_port=" + request.getLocalPort() +
+                "&s=" + s.getId() +
+                "&referer=" + request.getRequestURL()
+            );
+            return null;
+        }
+        return s.getAttribute("user").toString();
+    }
+}
