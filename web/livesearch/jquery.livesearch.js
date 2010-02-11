d98ac2f7fc0eb5222cec661ca03dbbef704af61d
mrkschan <mr.kschan@gmail.com>
2010-02-11 17:58:42 +0800

    liveUpdate will now dispatch event when hide/show

--- web/livesearch/jquery.livesearch.js
+++ web/livesearch/jquery.livesearch.js
@@ -23,16 +23,33 @@ jQuery.fn.liveUpdate = function(list, mapper){
 
 		if ( !term ) {
 			rows.show();
+
+			// dispatch show event
+			rows.each(function(i, el) {
+				$(el).trigger('liveupdate-show', $(el));
+			});
 		} else {
+/*
 			rows.hide();
 
 			cache.each(function(i){
 				var score = this.score(term);
 				if (score > 0) { scores.push([score, i]); }
 			});
-
 			jQuery.each(scores.sort(function(a, b){return b[0] - a[0];}), function(){
 				jQuery(rows[ this[1] ]).show();
+			});
+*/
+			cache.each(function(i)
+			{
+				var score = this.score(term);
+				if (score > 0) {
+					$(rows[i]).show();
+					$(rows[i]).trigger('liveupdate-show', $(rows[i]));
+				} else {
+					$(rows[i]).hide();
+					$(rows[i]).trigger('liveupdate-hide', $(rows[i]));
+				}
 			});
 		}
 	}
